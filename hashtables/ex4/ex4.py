aa8b3d8f56acf1bc6133d7c689daea21ffb69e9f
Nolan Picini <nolanpic@gmail.com>
2020-06-04 15:59:38 -0700

    complete ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,17 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # store the numbers that have negatives
+    result = []
+    # turn the list into a dictionary
+    # for fast lookup
+    num_dict = {num:True for num in a}
+    # loop thru input a,
+    for num in a:
+        # if the number is not negative,
+        # and the inverse of that number exists
+        # in the dict,
+        if num > 0 and (num*-1) in num_dict:
+            # add the num to the results
+            result.append(num)
 
     return result
 
